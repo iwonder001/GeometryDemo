c6fd94d9e3f8ea7121ac7a9fa1700f39280430fa
Kristen <kcarethers@gmail.com>
2016-12-27 17:00:23 -0500

    Created Geometry class and instance fields

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,7 +2,19 @@
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/**Exercises 9 (page 559) + UML class diagram - Geometry Calculator
+          
+//   Next, write a program called GeometryDemo to test the class, which displays the following menu and responds to the user’s selection:
+//     Geometry Calculator
+//1. Calculate the Area of a Circle
+//2. Calculate the Area of a Rectangle
+//3. Calculate the Area of a Triangle
+//4. Quit
+//Enter your choice (1-4):
+//Display an error message if the user enters a number outside the range of 1 through 4 when selecting an item from the menu.
+//Make sure to create a separate java file for each exercise.
+		 * 
+		 */
 
 	}
 
