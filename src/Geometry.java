fc792e7f1423091b2016b286304091aea3969180
Kristen <kcarethers@gmail.com>
2016-12-27 19:34:31 -0500

    Completed GeometryDemo main app and program now runs

--- src/Geometry.java
+++ src/Geometry.java
@@ -9,50 +9,53 @@ public class Geometry {
 	private static double areaC;
 	private static double areaR;
 	private static double areaT;
-	private double radius;
-	private double length;
-	private double width;
-	private double base;
-	private double height;
+//	private double radius;
+//	private double length;
+//	private double width;
+//	private double base;
+//	private double height;
 	
 	static Scanner input = new Scanner(System.in);
 	
-	public static double circleArea(double r){
+	public static double circleArea(){
 		System.out.println("What is the radius of your circle? ");
-		r = input.nextDouble();
+		double r = input.nextDouble();
 		if(r < 0) {
 			System.out.println("Error!!!  You must enter a positive value");
 		}else {	
 		areaC = Math.PI * r;
+		System.out.printf("Your area of your circle is " + "%.2f", areaC);
 		}
 		return areaC;
 		
 	}
-	public static double rectangleArea(double len, double wid){
+	public static double rectangleArea(){
 		System.out.println("What is the length of the rectangle? ");
-		len = input.nextDouble();
+		double len = input.nextDouble();
 		System.out.println("What is the width of the rectangle? ");
-		wid = input.nextDouble();
+		double wid = input.nextDouble();
 		if(len < 0){
 			System.out.println("Error!!!  You must enter a positive value");
 		}else if (wid < 0){
 			System.out.println("Error!!!  You must enter a positive value");
 		}else{
 		areaR = len * wid;
+		System.out.printf("Your area of your circle is " +"%.2f", areaR);
 		}
 		return areaR;
 	}
-	public static double triangleArea(double base, double height) {
+	public static double triangleArea() {
 		System.out.println("What is the base of the triangle? ");
-		base = input.nextDouble();
+		double base = input.nextDouble();
 		System.out.println("What is the height of the triangle? ");
-		height = input.nextDouble();
+		double height = input.nextDouble();
 		if(base <0){
 			System.out.println("Error!!!  You must enter a positive value");
 		}else if (height <0){
 			System.out.println("Error!!!  You must enter a positive value");
 		}else{
 		areaT = base * height *0.5;
+		System.out.printf("Your area of your circle is " +"%.2f", areaT);
 		}
 		return areaT;
 	}
--- src/MainApp.java
+++ src/MainApp.java
@@ -1,21 +1,51 @@
+import java.util.Scanner;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		/**Exercises 9 (page 559) + UML class diagram - Geometry Calculator
-          
-//   Next, write a program called GeometryDemo to test the class, which displays the following menu and responds to the user’s selection:
-//     Geometry Calculator
-//1. Calculate the Area of a Circle
-//2. Calculate the Area of a Rectangle
-//3. Calculate the Area of a Triangle
-//4. Quit
-//Enter your choice (1-4):
-//Display an error message if the user enters a number outside the range of 1 through 4 when selecting an item from the menu.
-//Make sure to create a separate java file for each exercise.
+		/**
+		 * Exercises 9 (page 559) + UML class diagram - Geometry Calculator
+		 * 
+		 * // Next, write a program called GeometryDemo to test the class, which
+		 * displays the following menu and responds to the user’s selection: //
+		 * Geometry Calculator //1. Calculate the Area of a Circle //2.
+		 * Calculate the Area of a Rectangle //3. Calculate the Area of a
+		 * Triangle //4. Quit //Enter your choice (1-4): //Display an error
+		 * message if the user enters a number outside the range of 1 through 4
+		 * when selecting an item from the menu. //Make sure to create a
+		 * separate java file for each exercise.
 		 * 
 		 */
+		// declare variables
+		Scanner input = new Scanner(System.in);
+		//Geometry gem = new Geometry();
+
+		System.out.println(
+				" Geometry Calculator \n1. Calculate the Area of a Circle \n2. Calculate the Area of a Rectangle"
+						+ "\n3. Calculate the Area of a Triangle \n4. Quit \nEnter your choice (1-4): ");
+
+		int choice = input.nextInt();
+		
+		
+			switch (choice) {
+			case 1:
+				Geometry.circleArea();
+				break;
+			case 2:
+				Geometry.rectangleArea();
+				break;
+			case 3:
+				Geometry.triangleArea();
+				break;
+			case 4:
+				System.out.println("You quit.  Bye");	
+				break;
+				default:
+					System.out.println("Error!!!  Please enter in a number between 1 - 4.");
+					break;
+			}
+
+		}
 
 	}
 
-}
